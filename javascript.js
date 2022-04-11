3c30bfa06f64a2b1cc2545d2404382ebb3a40546
frankmankut <workingclass.lib@protonmail.com>
2022-04-11 21:44:20 +0200

    modified:   index.html 	modified:   javascript.js - I wrote the first line of code

--- javascript.js
+++ javascript.js
@@ -1,1 +1,10 @@
-console.log("Hello world!")
+let computerPlay = Math.floor(Math.random()*3) + 1;
+    if (computerPlay == 1) {
+        console.log("Rock")
+    }
+    if (computerPlay == 2) {
+        console.log("Paper")
+    }
+    if (computerPlay == 3) {
+        console.log("Scissors")
+    }
